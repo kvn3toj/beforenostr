af4240c932bf5be7e45a22be22b23c7a7e9189cc
Kvn3Toj <kpp.coomunity@gmail.com>
2025-06-27 23:53:02 -0500

    Light

--- backend/src/generated/prisma/edge.js
+++ backend/src/generated/prisma/edge.js
@@ -918,7 +918,7 @@ const config = {
   },
   "relativeEnvPaths": {
     "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "schemaEnvPath": "../../../../.env"
   },
   "relativePath": "../../../prisma",
   "clientVersion": "6.10.1",
