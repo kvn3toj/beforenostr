e8383f19b6551b84ed8034b63627ffea9aed84cf
Kvn3Toj <kpp.coomunity@gmail.com>
2025-07-07 14:14:46 -0500

    chore: Staging final changes for forced sync - PHOENIX protocol completion

--- backend/src/generated/prisma/edge.js
+++ backend/src/generated/prisma/edge.js
@@ -955,6 +955,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- backend/src/generated/prisma/index.js
+++ backend/src/generated/prisma/index.js
@@ -956,6 +956,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
